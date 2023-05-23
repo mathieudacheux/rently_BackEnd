d2cc9c27dc6b5b82059b56223f96ef7a8db7b9ba
twinklose <mathieu.dacheux@icloud.com>
2023-05-23 22:11:32 +0200

    Remove log

--- src/controllers/pages/IndexController.ts
+++ src/controllers/pages/IndexController.ts
@@ -15,7 +15,6 @@ export class IndexController {
   @(Returns(200, String).ContentType("text/html"))
   get(@HeaderParams("x-forwarded-proto") protocol: string, @HeaderParams("host") host: string) {
     const hostUrl = `${protocol || "http"}://${host}`;
-    console.log(this.swagger);
     return {
       BASE_URL: hostUrl,
       docs: this.swagger.map((conf) => {
