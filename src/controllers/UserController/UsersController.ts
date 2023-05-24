2aea75e37e711b7b0594820a7b119d117ff62c60
twinklose <mathieu.dacheux@icloud.com>
2023-05-24 18:00:27 +0200

    eslint more strict

--- src/controllers/UserController/UsersController.ts
+++ src/controllers/UserController/UsersController.ts
@@ -2,13 +2,13 @@ import { Controller, Get, PathParams } from '@tsed/common'
 import { Inject } from '@tsed/di'
 import { PrismaService } from '../../services/PrismaService'
 import { Groups, Required, Email, Returns, Summary } from '@tsed/schema'
-// import { users } from '@prisma/client'
 
 class UserModel {
 	@Required()
 	@Groups('!creation')
 	id_users: number
 
+	@Required()
 	@Email()
 	mail: string
 }
@@ -18,24 +18,17 @@ export class UsersController {
 	@Inject()
 	protected prisma: PrismaService
 
-	@Get('/:id')
-	@Summary('Return a use by his id')
-	@Returns(200, UserModel)
-	async getUserById(@PathParams('id') id_users: number) {
-		return this.prisma.users.findUnique({ where: { id_users } })
-	}
-
-	// @Get('/:mail')
-	// @Summary('Return a use by his mail')
-	// @Returns(200, UserModel)
-	// async getUserByMail(@Email() mail: string) {
-	// 	return this.prisma.users.findUnique({ where: { mail } })
-	// }
-
 	@Get('/')
-	@Summary('Return a list of User')
+	@Summary('Return a list of all users')
 	@Returns(200, Array).Of(UserModel)
 	getUsers() {
 		return this.prisma.users.findMany()
+	}
+
+	@Get('/:id')
+	@Summary('Return a user by his id')
+	@Returns(200, UserModel)
+	async getUserById(@PathParams('id') id_users: number) {
+		return this.prisma.users.findUnique({ where: { id_users } })
 	}
 }
