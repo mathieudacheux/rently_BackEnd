6a1199c9cc86d11b9891bc9bace21f0517073548
twinklose <mathieu.dacheux@icloud.com>
2023-06-27 13:15:15 +0200

    Improvement on agencies and addresses

--- src/controllers/Address/AddressesController.test.ts
+++ src/controllers/Address/AddressesController.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { BASE_URL } from '../../config/index'
+import { getUserToken } from '../../helpers/helpersFunctions'
 
 describe('Addresses controller endpoint', () => {
 	const newAddress = {
@@ -13,24 +14,40 @@ describe('Addresses controller endpoint', () => {
 
 	let address_id: number
 	beforeAll(async () => {
-		address_id = (await request(BASE_URL).post('/addresses').send(newAddress)).body
-			.address_id
+		address_id = (
+			await request(BASE_URL)
+				.post('/addresses')
+				.set('Authorization', `Bearer ${await getUserToken()}`)
+				.send(newAddress)
+		).body.address_id
 	})
 	afterAll(async () => {
-		await request(BASE_URL).delete(`/addresses/${address_id}`)
+		await request(BASE_URL)
+			.delete(`/addresses/${address_id}`)
+			.set('Authorization', `Bearer ${await getUserToken()}`)
+			.send()
 	})
 	it('one address should return 200', async () => {
-		const response = await request(BASE_URL).get(`/addresses/${address_id}`)
+		const response = await request(BASE_URL)
+			.get(`/addresses/${address_id}`)
+			.set('Authorization', `Bearer ${await getUserToken()}`)
+			.send()
 		expect(response.statusCode).toBe(200)
 		expect(response.error).toBe(false)
 	})
 	it('all addresses should return 200', async () => {
-		const response = await request(BASE_URL).get('/addresses')
+		const response = await request(BASE_URL)
+			.get('/addresses')
+			.set('Authorization', `Bearer ${await getUserToken()}`)
+			.send()
 		expect(response.statusCode).toBe(200)
 		expect(response.error).toBe(false)
 	})
 	it('should return addresses', async () => {
-		const response = await request(BASE_URL).get('/addresses')
+		const response = await request(BASE_URL)
+			.get('/addresses')
+			.set('Authorization', `Bearer ${await getUserToken()}`)
+			.send()
 		expect(response.body.length >= 1).toBe(true)
 	})
 })
--- src/controllers/Agency/AgenciesController.test.ts
+++ src/controllers/Agency/AgenciesController.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { BASE_URL } from '../../config/index'
+import { getUserToken } from '../../helpers/helpersFunctions'
 
 describe('Agencies controller endpoint', () => {
 	const newAgency = {
@@ -10,23 +11,39 @@ describe('Agencies controller endpoint', () => {
 
 	let agency_id: number
 	beforeAll(async () => {
-		agency_id = (await request(BASE_URL).post('/agencies').send(newAgency)).body.agency_id
+		const response = await request(BASE_URL)
+			.post('/agencies')
+			.set('Authorization', `Bearer ${await getUserToken()}`)
+			.send(newAgency)
+		agency_id = response.body.agency_id
 	})
 	afterAll(async () => {
-		await request(BASE_URL).delete(`/agencies/${agency_id}`)
+		await request(BASE_URL)
+			.delete(`/agencies/${agency_id}`)
+			.set('Authorization', `Bearer ${await getUserToken()}`)
+			.send()
 	})
 	it('one agency should return 200', async () => {
-		const response = await request(BASE_URL).get(`/agencies/${agency_id}`)
+		const response = await request(BASE_URL)
+			.get(`/agencies/${agency_id}`)
+			.set('Authorization', `Bearer ${await getUserToken()}`)
+			.send()
 		expect(response.statusCode).toBe(200)
 		expect(response.error).toBe(false)
 	})
 	it('all agencies should return 200', async () => {
-		const response = await request(BASE_URL).get('/agencies')
+		const response = await request(BASE_URL)
+			.get('/agencies')
+			.set('Authorization', `Bearer ${await getUserToken()}`)
+			.send()
 		expect(response.statusCode).toBe(200)
 		expect(response.error).toBe(false)
 	})
 	it('should return agencies', async () => {
-		const response = await request(BASE_URL).get('/agencies')
+		const response = await request(BASE_URL)
+			.get('/agencies')
+			.set('Authorization', `Bearer ${await getUserToken()}`)
+			.send()
 		expect(response.body.length >= 1).toBe(true)
 	})
 })
