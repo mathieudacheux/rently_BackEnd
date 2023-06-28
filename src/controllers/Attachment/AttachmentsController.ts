218f03d344deb06c6010baa9ac23abc836f2ed5d
crigaux <crigaux057@gmail.com>
2023-06-28 13:40:40 +0200

    Added pdf in the endpoint File

--- src/controllers/Attachment/AttachmentsController.ts
+++ src/controllers/Attachment/AttachmentsController.ts
@@ -11,8 +11,8 @@ import { access, mkdir, unlink } from 'fs'
 import sharp from 'sharp'
 
 @Controller('/')
-export class AttachmentController {
-	@Post('/:folder/:id')
+export class Attachment {
+	@Post('/img/:folder/:id')
 	@MulterOptions({
 		storage: multer.diskStorage({
 			destination: async function (req, file, cb) {
@@ -34,7 +34,7 @@ export class AttachmentController {
 		}),
 		limits: { fileSize: 5000000 },
 	})
-	async uploadFile1(
+	async uploadFileImg(
 		@MultipartFile('file') file: PlatformMulterFile,
 		@PathParams('id') idUser: number,
 		@PathParams('folder') folder: string
@@ -59,6 +59,35 @@ export class AttachmentController {
 				throw new Error('Error during file deletion')
 			}
 		})
+		return { file: file, id_user: idUser }
+	}
+
+	@Post('/pdf/:folder/:id')
+	@MulterOptions({
+		storage: multer.diskStorage({
+			destination: async function (req, file, cb) {
+				const path = `./src/uploads/${req.params.folder}/${req.params.id}`
+				access(path, function (error) {
+					if (error) {
+						mkdir(path, (err) => {
+							if (err) {
+								throw new Error('Error during directory creation')
+							}
+						})
+					}
+					cb(null, path)
+				})
+			},
+			filename: function (req, file, cb) {
+				cb(null, file.originalname)
+			},
+		}),
+		limits: { fileSize: 5000000 },
+	})
+	async uploadFilePDF(
+		@MultipartFile('file') file: PlatformMulterFile,
+		@PathParams('id') idUser: number
+	) {
 		return { file: file, id_user: idUser }
 	}
 }
