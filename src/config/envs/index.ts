c753c48a30a8464e99860d28966e1ce3f20f42d2
twinklose <mathieu.dacheux@icloud.com>
2023-07-11 09:07:12 +0200

    Improvements

--- src/config/envs/index.ts
+++ src/config/envs/index.ts
@@ -4,4 +4,8 @@ export const envs = {
 	...process.env,
 	...dotenv.config().parsed,
 }
+
+if (process.env.NODE_ENV === 'development') {
+	console.log('envs', envs)
+}
 export const isProduction = process.env.NODE_ENV === 'production'
